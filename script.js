6744268dc0e06b24b53b20ddc98e34e11018a685
hari-priya-12 <142929475+hari-priya-12@users.noreply.github.com>
2023-08-23 10:42:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-
-  function toggleMenu() {
-    var menuLinks = document.querySelector(".menu-links");
-    menuLinks.classList.toggle("active");
-  }
-
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
